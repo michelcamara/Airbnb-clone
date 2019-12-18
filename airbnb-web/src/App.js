93cf5ab78d188d8dc81fd41c6e6a31b93fb08c3f
Michel Câmara <michelvictor16@gmail.com>
2019-12-18 17:55:30 -0400

    Airbnb-web

---
+++ airbnb-web/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Routes from './routes'
+import GlobalStyle from './styles/global'
+
+const App = () => (
+  <Fragment>
+    <Routes/>
+    <GlobalStyle/>
+  </Fragment>
+);
+
+export default App;
