a19da40ac8a57d64bb4ba70e6d325b2cc8b45b04
Michel Câmara <michelvictor16@gmail.com>
2019-12-27 14:58:24 -0400

    removido o meu token mapboxgl

--- AirbnbApp/src/index.js
+++ AirbnbApp/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Routes from './routes';
 import MapboxGL from '@mapbox/react-native-mapbox-gl';
-MapboxGL.setAccessToken('pk.eyJ1IjoibWljaGVsY2FtYXJhIiwiYSI6ImNrNGExMjkzbDBiMngzcnJ5MW9weHFyZTkifQ.s0x4iBB14Tzc5T-IF97jew');
+MapboxGL.setAccessToken('seu-token-aqui');
 
 const App = () => <Routes />;
 
